96a5e2aebb6a0ca487ed34382d08bfad39a40ebc
Aleksey Danilov <gggdarin@gmail.com>
2021-12-03 18:45:32 +0300

    переименовал проект, добавил проект WinAsynchMethod3.

---
+++ WinAsynchMethod3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAsynchMethod3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int Summ(int a, int b)
+        {
+            System.Threading.Thread.Sleep(9000);
+            return a + b;
+        }
+        private void CallBackMethod(IAsyncResult ar)
+        {
+            string str;
+            AsunchSumm summdelegate = (AsunchSumm)ar.AsyncState;
+            str = String.Format("Сумма введенных чисел равна {0}", summdelegate.EndInvoke(ar));
+            MessageBox.Show(str, "Результат операции");
+        }
+        public delegate int AsunchSumm(int a, int b);
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            try
+            {
+                a = Int32.Parse(txbA.Text);
+                b = Int32.Parse(txbB.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("При выполнении преобразования типов возникла ошмбка");
+                txbA.Text = txbB.Text = "";
+                return;
+            }
+            AsunchSumm summdelegate = new AsunchSumm(Summ);
+            AsyncCallback cb = new AsyncCallback(CallBackMethod);
+            summdelegate.BeginInvoke(a, b, cb, summdelegate);
+        }
+
+        private void btnWork_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Работа кипит!!!");
+        }
+    }
+}
